a340054af89685bb550cf80d29d98c5fea641ceb
scrviy <91909966+scrviy@users.noreply.github.com>
2023-03-10 14:50:12 -0300

    add static

---
+++ poo_static/index.js
@@ -0,0 +1,27 @@
+class ControleRemoto {
+    constructor(tv) {
+        this.tv = tv;
+        this.volume = 0;
+    }
+
+    //Método de instância
+    aumentarVolume() {
+        this.volume += 2;
+    }
+
+    diminuirVolume() {
+        this.volume -= 2;
+    }
+
+    //Método estático
+    static soma(x, y) {
+        return x + y;
+    }
+}
+
+const controle1 = new ControleRemoto("LG");
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+console.log(controle1);
+
+console.log(ControleRemoto.soma(2, 8));
